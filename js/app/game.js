1be222ba1b06bf81f4181005a959e877ebf6dd34
ZacharyKamerling <ZacharyKamerling@gmail.com>
2018-04-24 20:25:55 -0700

    Update

--- js/app/game.js
+++ js/app/game.js
@@ -105,11 +105,16 @@ class Game {
         for (let i = 0; i < this.unitPrototypes.length; i++) {
             let proto = this.unitPrototypes[i];
             if (proto.name === name) {
-                let imgs = new Array();
-                for (let img of proto.sprite_graphics) {
-                    imgs.push(img.img_ref);
+                if (proto.is_structure) {
+                    let imgs = new Array();
+                    for (let img of proto.sprite_graphics) {
+                        imgs.push(img.img_ref);
+                    }
+                    this.control = new Interaction.BuildOrder.BeingIssued(proto.width_and_height.w, proto.width_and_height.h, i, imgs);
                 }
-                this.control = new Interaction.BuildOrder.BeingIssued(proto.width_and_height.w, proto.width_and_height.h, i, imgs);
+                else {
+                    Interaction.TrainOrder.issue(this, proto);
+                }
             }
         }
     }
